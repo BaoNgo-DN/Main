856a650c027fffc8e50def37b56e7f8f0a7e7b9a
Bao <lequocbaongo@gmail.com>
2024-08-14 15:03:32 -0400

    configure terraform deploy project files

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -8,12 +8,13 @@ terraform {
     }
   }
 
-# backend block specifies where and how the Terraform state is stored.
+  # backend block specifies where and how the Terraform state file is stored.
   backend "s3" {
-    bucket         = "devops-recipe-app-tf-state-baongo-dn"
-    key            = "tf-state-setup"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "devops-recipe-app-tf-state-baongo-dn"
+    # Defines the path within the S3 bucket to store the state file
+    key     = "tf-state-setup"
+    region  = "us-east-1"
+    encrypt = true
 
     # Specifies a DynamoDB table used for state locking and consistency,
     # preventing simultaneous operations from multiple users or processes
@@ -24,7 +25,8 @@ terraform {
 #configures the AWS provider
 provider "aws" {
   region = "us-east-1"
-
+  
+  # define default tags that will be applied to all resources managed by this Terraform configuration
   default_tags {
     tags = {
       Environment = terraform.workspace
