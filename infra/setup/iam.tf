5f692121dc45d13522a6bf28d39586344ee99bb4
Bao <lequocbaongo@gmail.com>
2024-08-15 15:49:41 -0400

    added ECR IAM permission for CD user

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -55,5 +55,38 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
 }
 
 # draft policy by data block => turn policy into resource block => attach policy resource to the user
+# Policy for ECR access 
 
-   
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
+}
